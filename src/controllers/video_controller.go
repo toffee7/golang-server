77db1aea90f5eb2ea76a4101e6b3be01997eb950
Syed Tauseef Tanweer <tauseef@cohesity.com>
2018-07-23 14:34:59 +0530

    Add more videos

--- src/controllers/video_controller.go
+++ src/controllers/video_controller.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"../data"
 )
@@ -11,13 +12,18 @@ import (
 // GetVideos specifies handler function to fetch videos
 func GetVideos(w http.ResponseWriter, req *http.Request) {
 	videos := make([]data.Video, 0)
+	// List<data.Video> videos = new ArrayList<>();
+	video := data.Video{}
 
-	video := data.Video{
-		Name: "video1",
-		Link: "https://www.youtube.com/embed/BnYq7JapeDA",
+	counts := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}
+	for _, num := range counts {
+		video = data.Video{
+			Name: "video" + strconv.Itoa(num),
+			Link: "https://www.youtube.com/embed/BnYq7JapeDA",
+		}
+		videos = append(videos, video)
 	}
 
-	videos = append(videos, video)
 	fmt.Println(videos)
 
 	bytes, _ := json.Marshal(videos)
--- src/data/video.go
+++ src/data/video.go
@@ -2,6 +2,6 @@ package data
 
 // Video specifies details of a video object.
 type Video struct {
-	Name string `json:"name,omitempty"`
+	Name string `json:"videoName,omitempty"`
 	Link string `json:"link,omitempty"`
 }
